8d5d94c12f394c58b46e266c0c09ce4567681349
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-09-02 23:41:27 +0300

    updated anagram.rb

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
+class Anagram
+    attr_accessor :test
+
+    def initialize(word)
+      @test = word 
+    end   
+
+    def match(array)
+      array.select {|y| y.split("").sort == @test.split("").sort}
+    end   
+
+end
 # Your code goes here!
